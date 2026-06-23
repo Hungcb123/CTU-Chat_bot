6d3d1d81ffc13f8b8bba00af709014b1009f357c
hungcb123 <caotuonghung@gmail.com>
2026-06-23 13:49:07 +0700

    done 2

--- setup_venv.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Creating Python virtual environment...
-python -m venv venv
-echo Virtual environment created successfully in the "venv" directory.
-echo Activating virtual environment...
-call venv\Scripts\activate.bat
-cmd /k
